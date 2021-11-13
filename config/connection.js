b52cc198380f2c97eeca6f68493f65c82fd2137d
maplesyrupman <maplesyrupman@protonmail.com>
2021-11-13 00:12:24 -0500

    added logic to accomodate heroku jawsDB

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,18 @@ const Sequelize = require('sequelize')
 
 require('dotenv').config()
 
-const sequilize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-})
+let sequilize;
+
+if(process.env.JAWSDB_URL) {
+    sequelize= new Sequelize(process.env.JAWSDB_URL)
+} else {
+    sequelize = Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost', 
+        dialect: 'mysql',
+        port: 3306
+    })
+}
+
+
 
 module.exports = sequilize
