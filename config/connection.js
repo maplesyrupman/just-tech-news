67d54893af1f6911e22450c429de5b59067d6ae7
maplesyrupman <maplesyrupman@protonmail.com>
2021-11-16 18:40:50 -0500

    handlebars js

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,12 @@ const Sequelize = require('sequelize')
 
 require('dotenv').config()
 
-let sequilize;
+let sequelize;
 
 if(process.env.JAWSDB_URL) {
-    sequelize= new Sequelize(process.env.JAWSDB_URL)
+    sequelize = new Sequelize(process.env.JAWSDB_URL)
 } else {
-    sequelize = Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost', 
         dialect: 'mysql',
         port: 3306
@@ -16,4 +16,4 @@ if(process.env.JAWSDB_URL) {
 
 
 
-module.exports = sequilize
+module.exports = sequelize
