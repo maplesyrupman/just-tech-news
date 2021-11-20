ad229ddd11eaf587abbd0ad070a2c5cf8b9a17c8
maplesyrupman <maplesyrupman@protonmail.com>
2021-11-19 23:48:48 -0500

    login and out

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ const path = require('path')
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({})
 
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
+
 const app = express()
 const PORT = process.env.PORT || 3001
 
@@ -15,6 +28,7 @@ app.set('view engine', 'handlebars')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess))
 
 app.use(routes)
 
